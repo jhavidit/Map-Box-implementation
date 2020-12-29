6027486f107962d961ffddded10c64d31509256f
jhavidit <jhavidit@gmail.com>
2020-12-29 20:11:26 +0530

    map box init project

---
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.reminderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+
+class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+        setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.map_view)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync {
+            it.setStyle(Style.MAPBOX_STREETS){
+
+            }
+        }
+
+    }
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+           super.onSaveInstanceState(outState)
+        outState.let {
+           mapView?.onSaveInstanceState(outState)
+       }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+}
